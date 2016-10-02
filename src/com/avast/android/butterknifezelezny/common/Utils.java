b8da76461683f1cbd98d58d0a8f3170046ef1b82
Marek Słomnicki <marek@slomnicki.net>
2016-10-02 14:16:00 +0200

    Add configurable access level modifier

--- src/com/avast/android/butterknifezelezny/common/Utils.java
+++ src/com/avast/android/butterknifezelezny/common/Utils.java
@@ -290,7 +290,7 @@ public class Utils {
     }
 
     public static String getAccessLevel() {
-        return PropertiesComponent.getInstance().getValue(Settings.ACCESS_LEVEL, "protected");
+        return PropertiesComponent.getInstance().getValue(Settings.ACCESS_LEVEL, "");
     }
 
     /**
